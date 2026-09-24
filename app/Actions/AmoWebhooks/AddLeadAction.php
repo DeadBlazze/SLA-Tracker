4f49e2217519cb86fe31de1d41595e87a06ce11f
DeadBlazze <pgejmer171@gmail.com>
2026-09-24 16:53:25 +0300

    initial prod

--- app/Actions/AmoWebhooks/AddLeadAction.php
+++ app/Actions/AmoWebhooks/AddLeadAction.php
@@ -39,41 +39,41 @@ class AddLeadAction {
         ]);
 
         // Запрашиваем все поля сделки
-        $response = $client->get("/api/v4/leads/{$leadId}?with=source", [
-            'headers' => [
-                'Authorization' => "Bearer {$token}",
-            ],
-        ]);
+        // $response = $client->get("/api/v4/leads/{$leadId}?with=source", [
+        //     'headers' => [
+        //         'Authorization' => "Bearer {$token}",
+        //     ],
+        // ]);
 
-        $leadData = json_decode((string) $response->getBody(), true);
+        // $leadData = json_decode((string) $response->getBody(), true);
         
-        // Выдёргиваем source_phone АТС
-        $targetFields = ['Source phone', 'Номер sipuni'];
-        $sourcePhone = collect($leadData['custom_fields_values'] ?? [])
-            ->first(function ($field) use ($targetFields) {
-                $name = $field['field_name'] ?? null;
-                return in_array($name, $targetFields, true);
-            })['values'][0]['value'] ?? null;
+        // // Выдёргиваем source_phone АТС
+        // $targetFields = ['Source phone', 'Номер sipuni'];
+        // $sourcePhone = collect($leadData['custom_fields_values'] ?? [])
+        //     ->first(function ($field) use ($targetFields) {
+        //         $name = $field['field_name'] ?? null;
+        //         return in_array($name, $targetFields, true);
+        //     })['values'][0]['value'] ?? null;
 
-        // $advertisingSources = [
-        //     "89539396999"=>"Сайт",
-        //     "89210802077" => "ВК"
-        // ]
-        $createdAtFromLead = collect($leadData['custom_fields_values'])
-            ->first(function ($field) {
-                return $field['field_id'] === 731973;
-            })['values'][0]['value'];
-        $data = [
-            "amo_lead_id" => $leadData['id'],
-            "pipeline_id" => $leadData['pipeline_id'],
-            "status_id" => $leadData['status_id'],
-            "source_phone" => $sourcePhone,
-            "net_profit" => 0,
-            "created_at"=> Carbon::createFromTimestamp($timestamp)->toDateTimeString(),
-            "amo_source_name" => $leadData['_embedded']['source']['name'] ?? null,
-            "amo_source_id" => $leadData['_embedded']['source']['id'] ?? null
-        ];
-        error_log($dt);
-        $this->leads->add($data);
+        // // $advertisingSources = [
+        // //     "89539396999"=>"Сайт",
+        // //     "89210802077" => "ВК"
+        // // ]
+        // $createdAtFromLead = collect($leadData['custom_fields_values'])
+        //     ->first(function ($field) {
+        //         return $field['field_id'] === 731973;
+        //     })['values'][0]['value'];
+        // $data = [
+        //     "amo_lead_id" => $leadData['id'],
+        //     "pipeline_id" => $leadData['pipeline_id'],
+        //     "status_id" => $leadData['status_id'],
+        //     "source_phone" => $sourcePhone,
+        //     "net_profit" => 0,
+        //     "created_at"=> Carbon::createFromTimestamp($timestamp)->toDateTimeString(),
+        //     "amo_source_name" => $leadData['_embedded']['source']['name'] ?? null,
+        //     "amo_source_id" => $leadData['_embedded']['source']['id'] ?? null
+        // ];
+        // error_log($dt);
+        // $this->leads->add($data);
     }
 }
